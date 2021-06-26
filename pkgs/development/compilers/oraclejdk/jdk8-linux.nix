b904919a0a710bc0a2fc17631bc2e98b913117ad
Luis-Hebendanz <luis.nixos@gmail.com>
2021-06-26 19:25:01 +0200

    Patched jdk8

--- pkgs/development/compilers/oraclejdk/jdk8-linux.nix
+++ pkgs/development/compilers/oraclejdk/jdk8-linux.nix
@@ -1,8 +1,8 @@
 import ./jdk-linux-base.nix {
   productVersion = "8";
-  patchVersion = "281";
+  patchVersion = "291";
   sha256.i686-linux = "/yEY5O6MYNyjS5YSGZtgydb8th6jHQLNvI9tNPIh3+0=";
-  sha256.x86_64-linux = "hejH2nJIx0UPsQVWeniEHQlzWXhQd2wkpSf+sC7z5YY=";
+  sha256.x86_64-linux = "04w6kkkn9kawcnfhqv4mxbbzrkf58phnpw4fcm21lqnr3lp2s1f5";
   sha256.armv7l-linux = "oXbW8hZxesDqwV79ANB4SdnS71O51ZApKbQhqq4i/EM=";
   sha256.aarch64-linux = "oFH3TeIzVsFk6IZcDEHVDVJC7dSbGcwhdUH/WUXSNDM=";
   jceName = "jce_policy-8.zip";
