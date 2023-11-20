bcd6be7a5ab22c94c775945fb16a61b5867f15d2
Luis-Hebendanz <consulting@qube.email>
2023-11-20 22:21:57 +0100

    Bumped version to master

--- pkgs/development/python-modules/broadcaster/default.nix
+++ pkgs/development/python-modules/broadcaster/default.nix
@@ -8,17 +8,17 @@
 buildPythonPackage rec {
   pname = "broadcaster";
   version = "0.2.0";
-  format = "setuptools";
+  format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "encode";
     repo = "broadcaster";
-    rev = version;
-    hash = "sha256-sb6vOp9AOBs0ld1P3/Uc1jsiw9SUjGOsSdSnyF7IOzI=";
+    rev = "377b4043814ac88a21255af1cc12d5650c785082";
+    hash = "sha256-Bno+SXFrLprfN8798djI4GiUWkN5eZsOhxO/e+Ujv+I=";
   };
 
   propagatedBuildInputs = with python3.pkgs; [
-
+      hatchling
       anyio
       asyncpg
       aiokafka
